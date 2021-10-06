74daf37339b8045a8d48cdde2c98a8056212953f
Manifold AI Learning <52330707+manifoldailearning@users.noreply.github.com>
2021-10-06 17:25:10 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='MyPackage',
+version='0.1',
+description='My Fist Package created',
+url='#',
+author='auth',
+author_email='support@manifoldailearning.in',
+license='MIT',
+packages=['MyPackage'],
+zip_safe=False)
